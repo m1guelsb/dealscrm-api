c6e3379caf8bcfd62cd329c1f724b283fbafbf88
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-12-02 23:03:52 -0300

    [feat] add editCustomerDto

---
+++ src/customer/dto/editCustomer.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class EditCustomerDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+}
